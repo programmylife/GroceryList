8b6a8d70c7a4ba076baa64eef1e82b20f6b70ca4
Andrew Mshar <acmshar@gmail.com>
2017-10-24 21:45:21 -0400

    Cleanup.

--- Python/groc.py
+++ Python/groc.py
@@ -5,10 +5,14 @@ import datetime
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+def initialize_db():
+    client = boto3.client('dynamodb')
+    return client
 
 @app.route('/doneShopping', methods=['POST'])
 def done_shopping():
@@ -17,28 +21,25 @@ def done_shopping():
         #json_data = request.get_json() doesn't seem to work even when sending sample JSON of {'foo':'bar'}
 
         json_items = json.loads(request.data)
-        mark_list_as_shopped(initialize_db(), create_list_from_front_end(json_items))
+        create_new_list(initialize_db(), str(uuid.uuid4()), create_list_from_front_end(json_items), datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-    print 'sdjkfhajklsdf'
-    return 'Hello, World!'
+    return ''
 
 @app.route('/postList', methods=['POST'])
 def post_list():
     if request.method == 'POST':
-        #Can't get flask to recognize JSON data from fetch, so I'm getting it as a string, then parsing.
-        #json_data = request.get_json() doesn't seem to work even when sending sample JSON of {'foo':'bar'}
-
         json_items = json.loads(request.data)
         print (json_items)
 
         #if no GUID, make one, and create a new list in the DB with it.
+        #This shouldn't really happen since all should be 'latest' or have a real GUID...
         if json_items[0]["ListGUID"] == '':
             print "NO GUID"
             newGUID = 'latest'
-            create_new_list(initialize_db(), newGUID, create_list_from_front_end(json_items))
+            create_new_list(initialize_db(), newGUID, create_list_from_front_end(json_items),'null')
 
         elif json_items[0]["ListGUID"] == 'latest':
-            print "Got dat latest GUID: " + json_items[0]["ListGUID"]
+            print "latest GUID"
             update_list(initialize_db(), create_list_from_front_end(json_items) )
 
         else:
@@ -48,119 +49,6 @@ def post_list():
     #Is there something better I should return?
     return ""
 
-
-
-def initialize_db():
-    client = boto3.client('dynamodb')
-    return client
-
-#todo: do I need this? I think I really just want to use it for get latest list.
-def get_list(id):
-    #Both of the below work.
-
-    client = initialize_db()
-
-    response = client.query(
-        #IndexName='list',
-        Select='ALL_ATTRIBUTES',
-        TableName='GroceryList',
-        KeyConditionExpression='GUID = :v1',
-        ExpressionAttributeValues={
-        ':v1': {
-            'S': id,
-        },
-    },
-    )
-
-    # response = client.query(
-    #     #IndexName='list',
-    #     Select='ALL_ATTRIBUTES',
-    #     TableName='GroceryList',
-    #     KeyConditionExpression='GUID = :v1',
-    #     ExpressionAttributeValues={
-    #     ':v1': {
-    #         'S': 'f2100b8e-5a91-40d7-b664-1cf80a6f95a2',
-    #     },
-    # },
-    # )
-
-
-    # response = client.get_item(
-    #     Key={
-    #     'ListGUID': {
-    #         'S': '789345',
-    #     },
-    #     'CreateDate': {
-    #         'S': '2016-12-30T14:20:00',
-    #     },
-    # },
-    # TableName='GroceryTest2',
-    # )
-
-    return response
-
-#put a new list into the DB
-def create_new_list(client, id, items):
-
-    print items
-    print id
-
-    response = client.put_item(
-        TableName='GroceryList',
-        Item={
-            'GUID': {
-                'S': id
-            },
-            'ShopDate': {
-                'S': 'null'
-            },
-            'list': {
-                'L': items
-            },
-        }
-    )
-
-    print response
-
-
-def create_list_from_front_end(items):
-    item_list = [];
-    for item in items:
-        item_list.append({'S': item["text"]})
-    return item_list
-
-
-#todo: update the list variable to come from the API. Needs to pass in GUID, CreateDate, list for ExpressionAttributeValues
-def update_list(client, items):
-    #http://boto3.readthedocs.io/en/latest/reference/services/dynamodb.html#DynamoDB.Client.update_item
-
-    print items
-
-    response = client.update_item(
-    ExpressionAttributeValues={
-        ':l': {
-            'L': items
-        }
-    },
-    Key={
-        'GUID': {
-            'S': 'latest'
-        },
-        'ShopDate': {
-            'S': 'null'
-        },
-    },
-    TableName='GroceryList',
-    ExpressionAttributeNames={
-        '#L': 'list'
-    },
-    ReturnValues='ALL_NEW',
-    UpdateExpression='SET #L = :l',
-    )
-
-    print response
-
-#todo: implement
 @app.route('/getLatest')
 def get_latest_list():
     latest_list_dict = get_list('latest')
@@ -168,24 +56,67 @@ def get_latest_list():
 
     print latest_list
     return jsonify(latest_list)
-    #{'foo':'bar'}
 
-def mark_list_as_shopped(client, items):
+def get_list(id):
+    client = initialize_db()
+    response = client.query(
+        #IndexName='list',
+        Select='ALL_ATTRIBUTES',
+        TableName='GroceryList',
+        KeyConditionExpression='GUID = :v1',
+        ExpressionAttributeValues={
+            ':v1': {
+                'S': id,
+            },
+        },
+    )
+    return response
 
+#put a new list into the DB
+def create_new_list(client, id, items, shop_date):
     response = client.put_item(
         TableName='GroceryList',
         Item={
             'GUID': {
-                'S': str(uuid.uuid4())
+                'S': id
             },
             'ShopDate': {
-                'S': datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                'S': shop_date
             },
             'list': {
                 'L': items
             },
         }
     )
+    print response
 
-    print(response)
-    return ''
+def create_list_from_front_end(items):
+    item_list = [];
+    for item in items:
+        item_list.append({'S': item["text"]})
+    return item_list
+
+def update_list(client, items):
+    response = client.update_item(
+        ExpressionAttributeValues={
+            ':l': {
+                'L': items
+            }
+        },
+        Key={
+            'GUID': {
+                'S': 'latest'
+            },
+            'ShopDate': {
+                'S': 'null'
+            },
+        },
+        TableName='GroceryList',
+        ExpressionAttributeNames={
+            '#L': 'list'
+        },
+        ReturnValues='ALL_NEW',
+        UpdateExpression='SET #L = :l',
+    )
+
+    print response
